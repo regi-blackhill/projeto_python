2b9e11ae122c712be54af7636e21b917c5a47b22
Reginaldo Negromonte <reginaldo.negromonte@gmail.com>
2024-04-14 01:07:01 -0300

    Uso de aspas, caractere de escape e r.

---
+++ curso_otavio_miranda/aula3.py
@@ -0,0 +1,13 @@
+# Aspas simples
+print('Primeiro teste')
+print(1, 'Reggie Blackhill')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
