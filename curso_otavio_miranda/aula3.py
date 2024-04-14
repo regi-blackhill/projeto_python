19c1ed8ceedcbf4db0528b2e6f9f4e99c0902933
Reginaldo Negromonte <reginaldo.negromonte@gmail.com>
2024-04-14 01:08:28 -0300

    Uso de aspas, caractere de escape e r

--- curso_otavio_miranda/aula3.py
+++ curso_otavio_miranda/aula3.py
@@ -1,13 +1,25 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
 # Aspas simples
 print('Primeiro teste')
-print(1, 'Reggie Blackhill')
+print(1, 'Reggie "Blackhill"', sep='- ', end='\n\n')
 
 # Aspas duplas
-print("Luiz Otávio")
-print(2, "Luiz 'Otávio'")
+print("Segundo teste")
+print(2, "Reggie 'Blackhill'", sep='- ', end='\n\n')
 
-# Escape
-print("Luiz \"Otávio\"")
+# Escape: uma forma poder exibir aspas duplas dentro de uma str com aspas duplas
+print("Reggie \"Blackhill\"")
 
-# r
-print(r"Luiz \"Otávio\"")
+# r usado para expressões regulares, mas permite visualizar o caracter de escape
+print(r"Reggie \"Blackhill\"", end='\n\n')
+
+# melhor prática: se quiser exibir aspas duplas, inserir dentro de aspas simples e vice-versa
+print('Aspas "duplas" exibidas')
+print("Aspas 'simples' exibidas")
