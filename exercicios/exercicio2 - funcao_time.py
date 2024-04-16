bd31472099d568cfaf1fdda64c76f582b6964e39
Reginaldo Negromonte <reginaldo.negromonte@gmail.com>
2024-04-16 16:42:54 -0300

    Unindo códigos .py em .ipynb para melhor organização

--- exercicios/exercicio2 - funcao_time.py
+++
@@ -1,7 +0,0 @@
-import time
-
-t = int(input("Digite quantos segundos deseja: "))
-
-for i in range(t):
-    time.sleep(1)
-    print(i + 1)
--- exercicios/exercicio_funcao_time.py
+++ exercicios/exercicio_funcao_time.py
@@ -2,6 +2,6 @@ import time
 
 print("Vamos testar a função time")
 
-time.sleep(2)
+time.sleep(2) # espera pela quantidade de tempo em segundos para prosseguir com o programa.
 
 print("Fim do teste")
